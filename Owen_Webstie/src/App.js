1d0624e1f1c65b6e144be93eb00b4db520b8536f
Owen Barnett <owenbarnett29@gmail.com>
2024-08-17 10:44:05 -0700

    Frist chagnes'

---
+++ Owen_Webstie/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is a website to show my code and host the ohell scorecard.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
